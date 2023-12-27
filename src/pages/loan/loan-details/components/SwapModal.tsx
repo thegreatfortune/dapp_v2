0e1a4443a2de3d6325cbb14209a78b5b9a511773
samlrf <lirufeng627@gmail.com>
2023-12-27 09:52:39 +0800

    update to input

--- src/pages/loan/loan-details/components/SwapModal.tsx
+++ src/pages/loan/loan-details/components/SwapModal.tsx
@@ -186,24 +186,24 @@ const SwapModal: React.FC<IProps> = (props) => {
             <img src={FolCoin} alt="" className='mt-23 h25 w25 opacity-50' />
           </div>
         </div>
-        <div className='z-1 h125 w-full b-rd-6 bg-gray'>
-          <span className='ml-5 text-14'>you pay</span>
+        <div className='z-1 h125 w-full b-rd-6'>
+          <span className='ml-5 mt-20 text-14'>you pay</span>
           <div className='flex'>
-            <Input disabled={youPay.token !== 'USDC'} value={youPay.amount} className='ml-2 mt-20 h50 w-431 text-15' onChange={onSetYouPay} />
+            <Input disabled={youPay.token !== 'USDC'} value={youPay.amount} className='ml-2 mt-20 h50 w-431 text-15 text-#fff' onChange={onSetYouPay} />
             <span className='m-auto ml-5 mt-32 text-center'>{youPay.token}</span>
           </div>
         </div>
         <div className='absolute left-206 right-50% top-50% z-2 h40 w-full'>
-          <div className='z-3 h40 w40 transform border-1 b-#000 b-rd-6 b-solid bg-gray c-#fff'>
+          <div className='z-3 h40 w40 transform border-1 b-gray b-rd-6 b-solid c-#fff'>
             <RetweetOutlined onClick={onSwap} twoToneColor="#eb2f96" style={{ fontSize: '40px', margin: 'auto', color: '#08c', width: '40px', position: 'absolute' }} className='absolute z-2 m-auto transform transition-transform active:scale-95 hover:scale-105 !hover:c-pink' />
           </div>
         </div>
         {/* <div className='m-auto h40 w-472 transform b-rd-6 from-[#0154fa] to-[#11b5dd] bg-gradient-to-r text-center text-20 font-700 transition-transform active:scale-99 hover:scale-101 !hover:c-pink' onClick={onSwap}>↑↓Exchange</div> */}
         <div className='relative z-1 h10 w-full'></div>
-        <div className='z-1 h125 w-full b-rd-6 bg-gray'>
+        <div className='z-1 h125 w-full b-rd-6'>
           <span className='ml-5 text-14'>you receiver</span>
           <div className='flex'>
-            <Input disabled={youReceiver.token !== 'USDC'} value={youReceiver.amount} className='ml-2 mt-20 h50 w-431 text-15' onChange={onSetYouReceiver} />
+            <Input disabled={youReceiver.token !== 'USDC'} value={youReceiver.amount} className='ml-2 mt-20 h50 w-431 text-15 text-#fff' onChange={onSetYouReceiver} />
             <span className='m-auto ml-5 mt-32 text-center'>{youReceiver.token}</span>
           </div>
         </div>
